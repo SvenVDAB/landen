c0dd3fa28a806badd12023df51f592b7bce99cf5
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-02-26 23:55:23 +0100

    compileerproblemen ivm maven dependency mysql-connector-java

--- src/main/java/be/vdab/landen/controllers/IndexController.java
+++ src/main/java/be/vdab/landen/controllers/IndexController.java
@@ -1,24 +1,22 @@
 package be.vdab.landen.controllers;
 
+import be.vdab.landen.services.LandService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
-public class IndexController {
-    IndexController() throws IOException {
-/*        System.out.println("Besturingssysteem:" + System.getProperty("os.name"));
-        System.out.println("Gebruikersnaam:" + System.getProperty("user.name"));
-        System.out.println("Home directory van de gebruiker:" + System.getProperty("user.home"));*/
-        var homeDirectory = Path.of(System.getProperty("user.home"));
-        var bestand = homeDirectory.resolve("organisatie.txt");
-        if (!Files.exists(bestand)) {
-            Files.writeString(bestand, "VDAB");
-            System.out.println(bestand + " gemaakt");
-        }
+class IndexController {
+    private final LandService landService;
+
+    public IndexController(LandService landService) {
+        this.landService = landService;
     }
+
+    @GetMapping
+    public ModelAndView index() {
+       return new ModelAndView("index", "aantalLanden", landService.findAantal());
+   }
 }
---
+++ src/main/java/be/vdab/landen/services/LandService.java
@@ -0,0 +1,20 @@
+package be.vdab.landen.services;
+
+import be.vdab.landen.repositories.LandRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional(readOnly = true)
+public class LandService {
+    private final LandRepository repository;
+
+    public LandService(LandRepository repository) {
+        this.repository = repository;
+    }
+    public long findAantal() {
+        return repository.count();
+    }
+}
